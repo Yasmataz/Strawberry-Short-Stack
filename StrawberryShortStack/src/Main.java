c35a8f4d3fa708e99cc18e6e021dfee01b8c8ed2
Yasmataz <yasamin.ahmadzadeh@yahoo.com>
2016-06-01 10:33:21 -0400

    Read in
    Reads in file into array. Searches by ingredient, not working 100%
    though

--- StrawberryShortStack/src/Main.java
+++ StrawberryShortStack/src/Main.java
@@ -1,7 +1,41 @@
+import java.util.Scanner;
 
 public class Main {
+	private static String[][] dataBase;
+
 	public static void main(String[] args) {
-		System.out.println("gitHubYay");
-		System.out.println("hi there");
+		ReadIn read1 = new ReadIn();
+		dataBase = read1.getData();
+		searchRecipe();
+		Scanner in = new Scanner(System.in);
+		
+		String key = "";
+		while(!key.equals("end")) {
+			key = in.nextLine();
+			searchIngredient(key);
+		}
+		
+		System.out.println(searchRecipe());
+	}
+
+	public static void searchIngredient(String ingredient) {
+		for (int i = 0; i < dataBase.length; i++) {
+			for (int j = 0; j < dataBase[0].length; j++) {
+				if(dataBase[i][j] != null && dataBase[i][j].equals(ingredient)) {
+					dataBase[i][j] = null;
+				}
+			}
+		}
+	}
+	
+	public static String searchRecipe() {
+		for (int i = 0; i < dataBase.length; i++) {
+			A:for (int j = 0; j < 19; j++) {
+				if(dataBase[i][j+1] != null)
+					break A;
+				return dataBase[i][0];
+			}
+		}
+		return null;
 	}
 }
--- StrawberryShortStack/src/ReadIn.java
+++ StrawberryShortStack/src/ReadIn.java
@@ -1,4 +1,66 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.nio.file.*;
+import java.util.Arrays;
 
 public class ReadIn {
+	private static String[][] dataBase;
 
+	public ReadIn() {
+		LineNumberReader lnr;
+		try {
+			lnr = new LineNumberReader(new FileReader("DataBase.txt"));
+			lnr.skip(Long.MAX_VALUE);
+			dataBase = new String[lnr.getLineNumber() + 1][20];
+			lnr.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		read();
+	}
+
+	private void read() {
+		String csvFile = "DataBase.txt";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
+		String[] sep = null;
+		try {
+			br = new BufferedReader(new FileReader(csvFile));
+			for (int i = 0; i < dataBase.length; i++) {
+				
+				if ((line = br.readLine()) != null) {
+					// use comma as separator
+					sep = line.split(cvsSplitBy);
+					dataBase[i][0] = sep[0];
+					System.out.println(Arrays.toString(sep));
+
+				}
+
+				for (int j = 0; j < dataBase[0].length; j++) {
+					try {
+					dataBase[i][j+1] = sep[j+1];
+					} catch(IndexOutOfBoundsException e) {
+						if(j<19)
+							dataBase[i][j+1] = null;
+					}
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	//	System.out.println(Arrays.deepToString(dataBase));
+	}
+	
+	public String[][] getData(){
+		return dataBase;
+	}
 }
